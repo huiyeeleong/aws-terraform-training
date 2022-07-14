1cf67512a30a47e4ee90cdea11b6935458afd52e
Leong <hui.yee.leong@accenture.com>
2022-07-14 15:40:31 +1000

    Updated user-data

---
+++ terraform-aws/user-data/variables.tf
@@ -0,0 +1,16 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
