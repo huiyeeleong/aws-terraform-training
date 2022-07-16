0a99ec45027e388c47607f5b1ed4c29b9f3cc84d
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-07-17 09:15:42 +1000

    Updated S3

--- jenkins-packer-demo/s3.tf
+++ jenkins-packer-demo/s3.tf
@@ -1,13 +1,21 @@
+# resource "aws_s3_bucket" "terraform-state" {
+#   bucket = "terraform-state-${random_string.random.result}"
+
+#   tags = {
+#     Name = "Terraform state"
+#   }
+# }
+
 resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-${random_string.random.result}"
+  bucket = "terraform-state-hui-bucket"
 
   tags = {
     Name = "Terraform state"
   }
 }
 
-resource "random_string" "random" {
-  length  = 8
-  special = false
-  upper   = false
-}
+# resource "random_string" "random" {
+#   length  = 8
+#   special = false
+#   upper   = false
+# }
