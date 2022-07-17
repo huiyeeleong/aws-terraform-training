38a6cd515cc1fd72f711e1bd8345cf73651294c4
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-07-17 14:53:15 +1000

    Added new key

--- jenkins-packer-demo/instance.tf
+++ jenkins-packer-demo/instance.tf
@@ -50,17 +50,17 @@ resource "aws_volume_attachment" "jenkins-data-attachment" {
   skip_destroy = true
 }
 
-resource "aws_instance" "app-instance" {
-  count         = var.APP_INSTANCE_COUNT
-  ami           = var.APP_INSTANCE_AMI
-  instance_type = "t2.micro"
+# resource "aws_instance" "app-instance" {
+#   count         = var.APP_INSTANCE_COUNT
+#   ami           = var.APP_INSTANCE_AMI
+#   instance_type = "t2.micro"
 
-  # the VPC subnet
-  subnet_id = aws_subnet.main-public-1.id
+#   # the VPC subnet
+#   subnet_id = aws_subnet.main-public-1.id
 
-  # the security group
-  vpc_security_group_ids = [aws_security_group.app-securitygroup.id]
+#   # the security group
+#   vpc_security_group_ids = [aws_security_group.app-securitygroup.id]
 
-  # the public SSH key
-  key_name = "mykey"
-}
+#   # the public SSH key
+#   key_name = "mykey"
+# }
